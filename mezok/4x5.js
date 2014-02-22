3bcf2c0e7f75f5da843601afacdd6875b5dad0aa
Szluka <szlukaandras@gmail.com>
2014-02-22 16:20:49 +0100

    Update 4x5.js

--- mezok/4x5.js
+++ mezok/4x5.js
@@ -1,5 +1,5 @@
 mezo({
   nev: 'Házikó',
-  leiras: 'Szluka András háza! belépés csak palikának (neki is csak nevetéssel!)'
+  leiras: 'Szluka András háza! Belépés csak PALIkának (Neki is csak nevetésseL!)'
 })
 
