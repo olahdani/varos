0d2730aa1f53a61e552290ce9000ae53edbb9ef5
DoraVivienKitti <DoraVivienKitti@citromail.hu>
2014-02-22 16:29:27 +0100

    Update 3x4.js

--- mezok/3x4.js
+++ mezok/3x4.js
@@ -1,5 +1,5 @@
 mezo({
-  nev: 'Házikó',
-  leiras: 'Ez egy házikó'
-})
+  nev: 'Vivien lakása',
+  leiras: 'Ez egy rózsaszín tetős , lila színű , 4 ablakos , 2 emeletes online házikó! :-)   '
+}) 
 
