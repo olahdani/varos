132fa51b1fde1935d0afbd5c92d55fe122d5d3f3
Attila Maczak <attila@maczak.hu>
2014-02-26 08:09:14 -0500

    egyszerubb betoltes

--- betoltes.js
+++ betoltes.js
@@ -1,30 +1,15 @@
-letezoMezo = function(vizszintes, fuggoleges) {
-  toltsdBeAMezot(vizszintes, fuggoleges)
-}
-
-toltsdBeAMezot = function(vizszintes, fuggoleges) {
+function letezoMezo(vizszintes, fuggoleges) {
   // A fájlnév egyszerűen a koordinátákból képezzük.
-  fajlnev = vizszintes + 'x' + fuggoleges + '.js';
-  xhr = new XMLHttpRequest();
-  xhr.onload = function() {
-    // eval az egy nagyon veszélyes dolog, ezért nem szabad általában
-    // használni, mi viszont egy pár különleges dolgot fogunk itt csinálni
-    // majd.
+  fajlnev = "https://rawgithub.com/csorvas/varos/master/mezok/" + vizszintes + 'x' + fuggoleges + '.js';
+  var js = document.createElement("script");
+  js.type = "text/javascript";
+  js.src = fajlnev;
 
-    var mezo = function(opciok) { mezoKeszites(vizszintes, fuggoleges, opciok); }
-    try {
-      eval(xhr.responseText);
-    } catch (hiba) {
-      alert("A " + vizszintes + "x" + fuggoleges + " mező fájlban hibát találtam!")
-    }
-  }
-  xhr.open("GET", "https://rawgithub.com/csorvas/varos/master/mezok/" + fajlnev);
-  xhr.send();
+  document.body.appendChild(js);
 }
 
 alapKep = 'haz.png';
-
-mezoKeszites = function(vizszintes, fuggoleges, mezoOpciok) {
+function mezo(vizszintes, fuggoleges, mezoOpciok) {
   var mezo = document.createElement("div");
   mezo.classList.add('mezo');
   mezo.style.backgroundImage = "url(https://rawgithub.com/csorvas/varos/master/kepek/" + (mezoOpciok.kep || alapKep) + ")";
