c84990121e38d47997bfd7801711d048788bca9e
Attila Maczak <attila@maczak.hu>
2014-02-26 23:11:53 -0500

    frissites

--- betoltes.js
+++ betoltes.js
@@ -1,20 +1,78 @@
-function letezoMezo(vizszintes, fuggoleges) {
-  // A fájlnév egyszerűen a koordinátákból képezzük.
-  fajlnev = "/mezok/" + vizszintes + 'x' + fuggoleges + '.js';
+Array.prototype.feldolgozas = function(iterator) {
+  for (var i = 0, length = this.length; i < length; i++) {
+    iterator.call(null, this[i], i, this);
+  }
+}
+
+mezolista.feldolgozas(function(nev) {
+  fajlnev = "/mezok/" + nev + '.js';
   var js = document.createElement("script");
   js.type = "text/javascript";
   js.src = fajlnev;
-
   document.body.appendChild(js);
-}
+})
+
+szereplo = szereploBetolt({vizszintes: 1, fuggoleges: 1});
 
 alapKep = 'haz.png';
-function mezo(mezoOpciok) {
+mezok = [];
+function mezo(opciok) {
   var mezo = document.createElement("div");
   mezo.classList.add('mezo');
-  mezo.style.backgroundImage = "url(/kepek/" + (mezoOpciok.kep || alapKep) + ")";
-  mezo.style.left = (mezoOpciok.vizszintes * 64) + "px"
-  mezo.style.top = (mezoOpciok.fuggoleges * 64) + "px"
+  mezo.style.backgroundImage = "url(/kepek/" + (opciok.kep || alapKep) + ")";
+  mezo.style.left = (opciok.vizszintes * 64) + "px"
+  mezo.style.top = (opciok.fuggoleges * 64) + "px"
   document.body.appendChild(mezo);
+  mezok.push({mezo: mezo, vizszintes: vizszintes, fuggoleges: fuggoleges})
+}
+
+function szereploBetolt(opciok) {
+  var sz = {};
+
+  var mezo = document.createElement("div");
+  mezo.classList.add('szereplo');
+  mezo.style.backgroundImage = "url(/kepek/szereplo.png)";
+  mezo.style.left = (opciok.vizszintes * 64) + "px"
+  mezo.style.top = (opciok.fuggoleges * 64) + "px"
+  document.body.appendChild(mezo);
+
+  sz.mezo = mezo;
+
+  return sz;
+}
+
+document.onkeydown = function (e) {
+  e = e || window.event;
+  if (e.keyCode == '38') {
+    fel(szereplo);
+    lep(szereplo);
+  } else if (e.keyCode == '39') {
+    jobbra(szereplo);
+    lep(szereplo);
+  } else if (e.keyCode == '40') {
+    le(szereplo);
+    lep(szereplo);
+  } else if (e.keyCode == '37') {
+    balra(szereplo);
+    lep(szereplo);
+  }
+}
+
+function balra(szereplo) {
+  szereplo.mezo.style.left = szereplo.mezo.offsetLeft - 64 + "px";
+}
+function jobbra(szereplo) {
+  szereplo.mezo.style.left = szereplo.mezo.offsetLeft + 64 + "px";
+}
+function le(szereplo) {
+  szereplo.mezo.style.top = szereplo.mezo.offsetTop + 64 + "px";
+}
+function fel(szereplo) {
+  szereplo.mezo.style.top = szereplo.mezo.offsetTop - 64 + "px";
+}
+
+
+function lep(szereplo) {
+  // Itt folytatjuk!
 }
 
--- mezo_lista.js
+++ mezo_lista.js
@@ -1,12 +1,12 @@
 
-letezoMezo(5, 2)
-letezoMezo(2, 8)
-letezoMezo(2, 3)
-letezoMezo(2, 8)
-letezoMezo(3, 1)
-letezoMezo(3, 4)
-letezoMezo(4, 5)
-letezoMezo(5, 2)
-letezoMezo(5, 3)
-letezoMezo(5, 5)
-letezoMezo(6, 2)
+mezolista = [
+  '2x3-attila',
+  '2x8-gellert',
+  '3x1-david',
+  '3x4-vivien',
+  '4x5-andras',
+  '5x2',
+  '5x3-bettina',
+  '5x5-mark',
+  '6x2-oliver',
+];
