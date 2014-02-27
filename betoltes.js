d472eba202a8a47b6a8a0c030a0a19692ccaded9
Attila Maczak <attila@maczak.hu>
2014-02-26 22:18:36 -0500

    Kepek nagyitasa

--- betoltes.js
+++ betoltes.js
@@ -13,8 +13,8 @@ function mezo(mezoOpciok) {
   var mezo = document.createElement("div");
   mezo.classList.add('mezo');
   mezo.style.backgroundImage = "url(/kepek/" + (mezoOpciok.kep || alapKep) + ")";
-  mezo.style.left = (mezoOpciok.vizszintes * 32) + "px"
-  mezo.style.top = (mezoOpciok.fuggoleges * 32) + "px"
+  mezo.style.left = (mezoOpciok.vizszintes * 64) + "px"
+  mezo.style.top = (mezoOpciok.fuggoleges * 64) + "px"
   document.body.appendChild(mezo);
 }
 
