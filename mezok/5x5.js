17f8f282d1779ebfda5f95da7ed8812af82be3c7
Attila Maczak <attila@maczak.hu>
2014-02-26 22:17:56 -0500

    hibajavitas

--- mezok/5x5.js
+++ mezok/5x5.js
@@ -2,6 +2,6 @@ mezo({
   vizszintes: 5,
   fuggoleges: 5,
   nev: 'Gte Ash',
-  leiras: 'Ez a ház Ash-jé! Ne lépj be! Vááááááá! Fuck You!
+  leiras: 'Ez a ház Ash-jé! Ne lépj be! Vááááááá! Fuck You!'
 })
 
