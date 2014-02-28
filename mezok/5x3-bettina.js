128f69ad77d6c2134e09e416c2a4544566c7dcb9
hamcsa <csaba.hamar@freemail.hu>
2014-02-28 17:26:46 +0100

    Update 5x3-bettina.js

--- mezok/5x3-bettina.js
+++ mezok/5x3-bettina.js
@@ -1,21 +1,12 @@
 mezo({
-  vizszintes: 5,
-  fuggoleges: 2,
-  nev: 'Bettina2003 háza',
-  leiras: 'Ez egy ház',
-  ralepeskor: function () {
-    
-  nev = parancs("Add meg a neved!");
-  
-  koszones = "Szia" +nev;
-  
-  kiir (koszones);
-  
-  szam = kerdez ("Mennyi 152+848?");
-  
-  if (szam ==1000) kiir ("Helyes!")
-  
-  else kiir ("Helytelen!");
+  mezo({
+  vizszintes: 8,
+  fuggoleges: 4,
+  nev: ' Betti Házikója',
+  leiras: 'Kicsi házikó.',
+  ralepeskor: function() {
+    kiir("Csaba házában vagy!");
   }
-})
-
+  
+});
+  
