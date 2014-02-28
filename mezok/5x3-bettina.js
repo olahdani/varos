b1983a5360ce802b813081bdd85298e627632939
Bettina2003 <szbetti2003@gmail>
2014-02-28 15:55:20 +0100

    Update 5x3-bettina.js

--- mezok/5x3-bettina.js
+++ mezok/5x3-bettina.js
@@ -3,5 +3,19 @@ mezo({
   fuggoleges: 2,
   nev: 'Bettina2003 háza',
   leiras: 'Ez egy ház'
+  ralapeskor : function () {
+    
+  nev = parancs("Add meg a neved!");
+  
+  koszones = "Szia" +nev;
+  
+  kiir (koszones);
+  
+  szam = kérdez ("Menny 152+948?");
+  
+  if (szam ==1000) kiir ("Helyes!")
+  
+  else kiir ("Helytelen!");
+  }
 })
 
