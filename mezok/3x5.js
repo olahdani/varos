1ec5304cdee4bda4e58b5143074fc881eb4ede9d
Attila Maczak <attila@maczak.hu>
2014-02-28 09:41:36 -0500

    uj hazikok

---
+++ mezok/3x5.js
@@ -0,0 +1,8 @@
+mezo({
+  vizszintes: 3,
+  fuggoleges: 5,
+  nev: ' Házikója',
+  leiras: 'Kicsi házikó.',
+  ralepeskor: function() {
+  }
+);
---
+++ mezok/5x1-csaba.js
@@ -0,0 +1,8 @@
+mezo({
+  vizszintes: 5,
+  fuggoleges: 1,
+  nev: ' Házikója',
+  leiras: 'Kicsi házikó.',
+  ralepeskor: function() {
+  }
+);
