2395df88531690995de01f35b4888d956957e179
hamcsa <csaba.hamar@freemail.hu>
2014-02-28 17:21:45 +0100

    Update 6x2-oliver.js

--- mezok/6x2-oliver.js
+++ mezok/6x2-oliver.js
@@ -1,14 +1,11 @@
 mezo({
-  vizszintes: 6,
-  fuggoleges: 2,
-  nev: 'Oli   Házikója'
-  leiras: 'Kicsi házikó'
+  vizszintes: 5,
+  fuggoleges: 1,
+  nev: ' Házikója',
+  leiras: 'Kicsi házikó.',
   ralepeskor: function() {
-  nev = kerdes ("Mi a neved?" );
-  udvozlet = "Szia " + nev;
-  kiir(udvozlet)
-  szam = kerdez("Hány óra egy nap?");
-  if (szam == 60)kiir(Helyes !");
-  else kiir("Nem helyes !");
-  }  
-})
+    kiir("Oli házában vagy!");
+  }
+  
+});
+  
