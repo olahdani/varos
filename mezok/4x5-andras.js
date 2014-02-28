bf6ea0b1dc106c33938c9c2d761f089164173cd5
Szluka <szlukaandras@gmail.com>
2014-02-28 16:04:22 +0100

    Update 4x5-andras.js

--- mezok/4x5-andras.js
+++ mezok/4x5-andras.js
@@ -2,7 +2,7 @@ mezo({
   vizszintes: 4,
   fuggoleges: 5,
   nev: 'Szluka A.',
-  leiras: 'Cottage'
+  leiras: 'Cottage',
 ralepeskor: function() {
  nev = kerdez("Milyen fajhoz tartozol?");
 udvozlet = "Nem csípem a fajod: " + nev;
