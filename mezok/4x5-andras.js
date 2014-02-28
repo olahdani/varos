b711bfc559e186f9b5de57418e2133fb8c84b000
Szluka <szlukaandras@gmail.com>
2014-02-28 17:23:57 +0100

    Update 4x5-andras.js

--- mezok/4x5-andras.js
+++ mezok/4x5-andras.js
@@ -5,7 +5,7 @@ mezo({
   leiras: 'Cottage',
 ralepeskor: function() {
  nev = kerdez("Szluka András vagyok!!! Milyen fajhoz tartozol?");
-udvozlet = "Oké" + nev;
+udvozlet = "Oké " + nev ", maradj csendes határaimon belül!";
  kiir(udvozlet);
 }
 })
