51c491d83c02f3e010d5ed4c47f53e7a528e32c8
Szluka <szlukaandras@gmail.com>
2014-02-28 17:27:41 +0100

    Update 4x5-andras.js

--- mezok/4x5-andras.js
+++ mezok/4x5-andras.js
@@ -4,7 +4,7 @@ mezo({
   nev: 'andras',
   leiras: 'Cottage',
 ralepeskor: function() {
- kiír 'András házában vagy!'
+ kiír ("András házában vagy!")
  nev = kerdez("Szluka András vagyok!!! Milyen fajhoz tartozol?");
 udvozlet = "Oké " + nev;
  kiir(udvozlet);
