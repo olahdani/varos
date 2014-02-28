41c2b7911844dbe5a9d8efb57736bde0b78b04f7
win88 <uoli2@citromail.hu>
2014-02-28 17:27:58 +0100

    Update 6x2-oliver.js

--- mezok/6x2-oliver.js
+++ mezok/6x2-oliver.js
@@ -5,7 +5,14 @@ mezo({
   leiras: 'Kicsi házikó.',
   ralepeskor: function() {
     kiir("Oli házában vagy!");
-  }
+  nev = kerdez("Mi a neved?");
+  udvozlet = "Szia " + nev;
+  kiir(udvozlet);
+   szam = kerdez("Hány óra egy nap?");
+   if (szam == 60) kiir("Helyes !");
+   else kiir("Nem helyes!");
+  
+   }
   
 });
   
