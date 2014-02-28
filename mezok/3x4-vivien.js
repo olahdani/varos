75920c76e4d9fab1a8bc0a1dc0999106e92fc86d
markk55 <ATordaiMark@gmail.com>
2014-02-28 15:40:52 +0100

    Update 3x4-vivien.js

--- mezok/3x4-vivien.js
+++ mezok/3x4-vivien.js
@@ -2,6 +2,6 @@ mezo({
   vizszintes: 3,
   fuggoleges: 4,
   nev: 'Vivien lakása',
-  leiras: 'Ez egy rózsaszín tetős , lila színű , 4 ablakos , 2 emeletes online házikó! :-)   '
+  leiras: 'Ez egy rózsaszín tetős , lila színű , 4 ablakos , 2 emeletes online házikó!lalalala!! :-)   '
 }) 
 
