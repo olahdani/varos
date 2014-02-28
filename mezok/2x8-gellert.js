0188e660a9c9b7f5ef3da2f9a88baf122f0dca5e
szgellert <oscarwind1@gmail.com>
2014-02-28 17:28:44 +0100

    Update 2x8-gellert.js

--- mezok/2x8-gellert.js
+++ mezok/2x8-gellert.js
@@ -5,21 +5,20 @@ mezo({
   leiras: 'Ez egy házikó'
 ralepeskor: function() {
 
-    // kerdez funkció feltesz egy kérdést a felhasználónak
-    // A választ egy változóban mentjük el.
+  
     nev = kerdez("Mi a neved?");
 
-    // Ha hozzá szeretnénk tenni a változóhoz, egyszerűen hozzáadhatjuk.
+   
     udvozlet = "Szia " + nev;
 
-    // A kiir metódussal tudunk szöveget kiírni.
+  
     kiir(udvozlet);
 
     szam = kerdez("Hány perc egy óra?");
 
-    // if jelentése: ha
+  
     if (szam == 60) kiir("Helyes !");
-    // else jelentése: különben
+   
     else kiir("Nem helyes!");
 }
 })
