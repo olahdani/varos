0353aff3ad3bba9c586c2a5a0387922d56c4376c
Szluka <szlukaandras@gmail.com>
2014-03-07 14:36:54 +0100

    Update 4x5-andras.js

--- mezok/4x5-andras.js
+++ mezok/4x5-andras.js
@@ -11,9 +11,6 @@ udvozlet = "Szervusz " + nev;
 szam = kerdez("Hány Euro kb.900FT? (!egy számot kérek!)");
 if (szam = 3) kiir("Tájékozott vagy, ezt szeretem! :D");
 else kiir("Ez helytelen megoldás! ]:(");
-sza = kerdez("Hány Euro kb.150 000FT? (!egy számot kérek!)");
-if (sza = 500) kiir("Ügyes! XD");
-else kiir("Ez helytelen megoldás! >:P");
 }
 })
 
