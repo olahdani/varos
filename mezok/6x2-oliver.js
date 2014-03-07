b9571b351f9f70944839c6bf42574873c63385f0
win88 <uoli2@citromail.hu>
2014-03-07 15:05:04 +0100

    Update 6x2-oliver.js

--- mezok/6x2-oliver.js
+++ mezok/6x2-oliver.js
@@ -14,6 +14,9 @@ mezo({
   szam = kerdez("Mennyi 1+1+1x7?");
    if (szam ==9 ) kiir("Helyes !");
   else kiir("Nem helyes!"); 
+    szam = kerdez("Mi a kedvenc számod??");
+   if (szam == ) kiir("OK !");
+  else kiir("Nem helyes!"); 
   
   
    }
