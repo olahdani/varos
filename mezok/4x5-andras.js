f81ec9e6c5f0d2c817a562e15248a144ad1e168d
Szluka <szlukaandras@gmail.com>
2014-03-07 14:29:49 +0100

    Update 4x5-andras.js

--- mezok/4x5-andras.js
+++ mezok/4x5-andras.js
@@ -5,12 +5,15 @@ mezo({
   leiras: 'Cottage',
 ralepeskor: function() {
  kiir ("András házában vagy!");
- nev = kerdez("Szluka András vagyok!Mi a neved?");
+ nev = kerdez("Szluka András vagyok! Mi a neved?");
 udvozlet = "Szervusz " + nev;
  kiir(udvozlet);
-szam = kerdez("hány € kb.900FT? (!egy számot kérek!)");
+szam = kerdez("Hány Euro kb.900FT? (!egy számot kérek!)");
 if (szam = 3) kiir("Tájékozott vagy, ezt szeretem! :D");
 else kiir("Ez helytelen megoldás! ]:(");
+sza = kerdez("Hány Euro kb.150 000FT? (!egy számot kérek!)");
+if (sza = 500) kiir("Ügyes! XD");
+else kiir("Ez helytelen megoldás! >:P");
 }
 })
 
