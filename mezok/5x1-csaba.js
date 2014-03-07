acfcd2e10722ab85f92dfba4b0256bf59a362720
hamcsa <csaba.hamar@freemail.hu>
2014-03-07 16:52:16 +0100

    Update 5x1-csaba.js

--- mezok/5x1-csaba.js
+++ mezok/5x1-csaba.js
@@ -4,6 +4,7 @@ mezo({
   nev: ' Házikója',
   leiras: 'Kicsi házikó.',
  ralepeskor: function() {
+    kiir ("Csaba házában vagy.");
     hany = kerdez("Hanyszor irjam le?");
         for (szamlalo = 0; szamlalo < hany; szamlalo++) {
         kiir("Minden nap programozni fogok!")
