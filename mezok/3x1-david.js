99581a3dc022f5ce0701752ee1a4a411764a56f4
szgellert <oscarwind1@gmail.com>
2014-03-07 12:24:44 +0100

    Update 3x1-david.js

--- mezok/3x1-david.js
+++ mezok/3x1-david.js
@@ -21,7 +21,7 @@ ralepeskor: function() {
   
     if (cim != "") kiir("Köszi, hogy beírtad!");
     
-        kiir("Adataid: "+ nev);
+       // kiir("Adataid: "+ nev);
    
     else kiir("Mért nem írtad be?");
 }
