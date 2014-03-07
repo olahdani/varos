4c5ad036f015b7631df1dcf9ad5cfa9032174999
hamcsa <csaba.hamar@freemail.hu>
2014-03-07 17:28:57 +0100

    Update 5x2-pali.js

--- mezok/5x2-pali.js
+++ mezok/5x2-pali.js
@@ -2,13 +2,13 @@ mezo({
   vizszintes: 5,
   fuggoleges: 2,
   nev: 'Pali Házikó',
-  leiras: 'kicsi házikó'
+  leiras: 'kicsi házikó',
   ralepeskor: function() {
   kiir('Pali házára léptél és fájt neki');
   nev = kerdez("Mi a neved?");
   udvozlet = "Szia " + nev;
-  szamlalo = kerdez("Hányszor írja ki?");
-  for (szamlalo = 0; szamlalo < almak; szamlalo++) {
+  szam = kerdez("Hányszor írja ki?");
+  for (szamlalo = 0; szamlalo < szam; szamlalo++) {
       kiir("Minden nap programozni fogok")
     }
 })
