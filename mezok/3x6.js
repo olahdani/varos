bd260b6bab7165d2c640de9ae859121728bb2db6
olahdani <vebcsavargo@gmail.com>
2014-03-07 16:07:17 +0100

    Update 3x6.js

---
+++ mezok/3x6.js
@@ -0,0 +1,15 @@
+mezo({
+  vizszintes: 3,
+  fuggoleges: 6,
+  nev: 'Dani Házikója',
+  leiras: 'Kicsi házikó.',
+  ralepeskor: function()
+  kiir("Üdvözöllek! Dani házában vagy.")
+  
+  
+  
+  
+  
+  {
+  }
+});
