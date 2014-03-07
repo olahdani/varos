ebb1211c8f1a96b9f6f5449caf20867f118b47ee
szgellert <oscarwind1@gmail.com>
2014-03-07 12:16:39 +0100

    Update 3x1-david.js

--- mezok/3x1-david.js
+++ mezok/3x1-david.js
@@ -2,5 +2,25 @@ mezo({
   vizszintes: 2,
   fuggoleges: 8,
   nev: 'Dávid háza',
-  leiras: 'Ez egy házikó'
+  leiras: 'Ez egy házikó',
+
+ralepeskor: function() {
+
+    kiir("Szia! Gellért házában vagy.");
+    
+    nev = kerdez("Mi a neved?");
+
+   
+    udvozlet = "Szia " + nev;
+
+  
+    kiir(udvozlet);
+
+    szam = kerdez("Hány éves vagy?");
+
+  
+    if (szam >= 80) kiir("Nem lehetsz ilyen öreg!");
+   
+    else kiir("Köszi!");
 }
+});
