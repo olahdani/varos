374d1d67d840f98f8cee96fd5f2630826657c659
olahdani <vebcsavargo@gmail.com>
2014-03-07 17:20:22 +0100

    Update 3x6.js

--- mezok/3x6.js
+++ mezok/3x6.js
@@ -4,12 +4,9 @@ mezo({
   nev: ' Házikója',
   leiras: 'Kicsi házikó.',
   ralepeskor: function() {
- kiir("Üdvözöllek! Dani házában vagy!"); 
-  nev = kerdez("Hogy hívnak?");
-  udvozlet = "szia " + nev;
-  kiir(udvozlet);
+      kiir("Üdvözöllek! Dani házában vagy!"); 
+      nev = kerdez("Hogy hívnak?");
+      udvozlet = "szia " + nev;
+      kiir(udvozlet);
   
-  email =(" Van e-mail címed?");
-  udv2 = "Az jó hogy" + email;
-  }
 });
