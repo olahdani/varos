69fbeb3974d2c68e6464358c2dbea82572d4ca5a
Attila Maczak <attila@maczak.hu>
2014-03-07 10:14:40 -0500

    Update 2x3-attila.js

--- mezok/2x3-attila.js
+++ mezok/2x3-attila.js
@@ -8,17 +8,7 @@ mezo({
     // Ciklus, haromszor hajtodik vegre
     for (szamlalo = 0; szamlalo < 3; szamlalo++) {
       
-      szam = kerdez("Hány éves vagy?");
-  
-      // if jelentése: ha
-      if (szam > 80) {
-        kiir("Nem lehetsz ilyen öreg!");
-        kiir("Fiatalabb vagy!");
-      }
-      // else jelentése: különben
-      else {
-        kiir("Fiatal vagy!");
-      }
+      kiir("Hanyadik futas: " + szamlalo)
     }
   }
 })
