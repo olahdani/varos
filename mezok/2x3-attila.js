c1313580487ff7a973311d8cbee0f2d74f1a8dd4
Attila Maczak <attila@maczak.hu>
2014-03-07 10:18:18 -0500

    Update 2x3-attila.js

--- mezok/2x3-attila.js
+++ mezok/2x3-attila.js
@@ -5,10 +5,10 @@ mezo({
   leiras: 'Kicsi házikó.',
   kep: 'haz2.png',
   ralepeskor: function() {
+    almak = kerdez("Hany almát szeretnél?");
     // Ciklus, haromszor hajtodik vegre
-    for (szamlalo = 0; szamlalo < 3; szamlalo++) {
-      
-      kiir("Hanyadik futas: " + szamlalo)
+    for (szamlalo = 0; szamlalo < almak; szamlalo++) {
+      kiir("Alma")
     }
   }
 })
