38f15c0852af65caa6bdce74d78fdb5a76ebe40e
olahdani <vebcsavargo@gmail.com>
2014-03-07 17:26:36 +0100

    Update 3x6.js

--- mezok/3x6.js
+++ mezok/3x6.js
@@ -6,8 +6,6 @@ mezo({
   kep: 'haz2.png',
   ralepeskor: function() {
       kiir("Üdvözöllek! Dani házában vagy!"); 
-      nev = kerdez("Hogy hívnak?");
-      udvozlet = "szia " + nev;
-      kiir(udvozlet);
+ 
   }
 });
