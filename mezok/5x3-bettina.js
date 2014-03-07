b982b0b1ca4dd3025ff29935e3cbab0b61b5ad8d
Bettina2003 <szbetti2003@gmail>
2014-03-07 15:35:26 +0100

    Update 5x3-bettina.js

--- mezok/5x3-bettina.js
+++ mezok/5x3-bettina.js
@@ -9,10 +9,10 @@ mezo({
     nev = kerdez ("Mi a keresztneved?");
     udvozlet = "szia "+nev;
     kiir (udvozlet);
-    kerdez1 = ("Hány éves vagy?");
-    kerdez2 = ("Hanyadik osztályos vagy?");
-    kerdez3 = ("Mi a kedvenc színed?");
-    kerdez4 = ("Szeretsz iskolába járni?");
+    kerdez1 = kerdez ("Hány éves vagy?");
+    kerdez2 = kerdez ("Hanyadik osztályos vagy?");
+    kerdez3 = kerdez ("Mi a kedvenc színed?");
+    kerdez4 = kerdez ("Szeretsz iskolába járni?");
     kiir (kerdez1)
     kiir (kerdez2)
     kiir (kerdez3)
