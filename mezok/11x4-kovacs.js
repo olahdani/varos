48fd4db43a4fc2f5e30ddc89b8c187c12792b459
Szluka <szlukaandras@gmail.com>
2014-03-09 12:05:29 +0100

    Update 11x4-kovacs.js

--- mezok/11x4-kovacs.js
+++ mezok/11x4-kovacs.js
@@ -12,6 +12,12 @@ M = 30;
     if (e == "igen"){
     kiir ("Köszönöm!");
 M = M - 5;
+     kiir M("$-od maradt.");
+    v = v + 1;
     }
+else (e == "nem") {
+        kiir ("Rendben, remélem máskor veszel!");
+    }
+ kiir (v)("vasrudad van."); 
 }
 })
