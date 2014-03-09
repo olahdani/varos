bf43d330f03122f56ea94562a1264cb80afa263a
Szluka <szlukaandras@gmail.com>
2014-03-09 11:48:08 +0100

    Create 11x4kovacs.js

---
+++ mezok/11x4kovacs.js
@@ -0,0 +1,22 @@
+mezo({
+  vizszintes: 11,
+  fuggoleges: 4,
+  nev: 'andras',
+  leiras: 'X',
+kep: 'haz3.png',
+ralepeskor: function() {
+ M = 30;
+ kiir M ("$");
+ kiir ("Jónapot!");
+  e = kerdez("Veszel egy vasrudat?");
+    if (e == "igen"){
+    kiir ("Köszönöm!");
+    M = M-5;
+    kiir M("$-od maradt.");
+    v = v+1;
+    }
+    else (e == "nem")
+        kiir ("Rendben, remélem máskor veszel!");
+ kiir v (" vasrudad van.");
+}
+})
