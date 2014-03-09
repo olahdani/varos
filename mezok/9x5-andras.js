96c571736620fc6412d5ad2604d166fa7128a596
Szluka <szlukaandras@gmail.com>
2014-03-09 11:38:46 +0100

    Update 9x5-andras.js

--- mezok/9x5-andras.js
+++ mezok/9x5-andras.js
@@ -7,11 +7,9 @@ kep: 'haz2.png',
 ralepeskor: function() {
  kiir ("Itt vagy nálam");
   d = kerdez("Alszol nálam?");
-    if (d == "igen"){
-    kiir ("Oké! ZzZzzZZ.")
-        }
-    else (d == "nem"){
-        kiir ("Jó, de én mentem aludni. ZzZzzZZ.")
-        }
+    if (d == "igen")
+    kiir ("Oké! ZzZzzZZ.");
+    else (d == "nem")
+        kiir ("Jó, de én mentem aludni. ZzZzzZZ.");
 }
 })
