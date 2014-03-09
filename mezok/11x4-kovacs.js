49ef81500f48adfb5877ab04812f2cf3a22c587a
Szluka <szlukaandras@gmail.com>
2014-03-09 11:55:46 +0100

    Update 11x4-kovacs.js

--- mezok/11x4-kovacs.js
+++ mezok/11x4-kovacs.js
@@ -6,18 +6,18 @@ mezo({
 kep: 'haz3.png',
 ralepeskor: function() {
  M = 30;
- kiir M ("$");
+ kiir (M);
  kiir ("Jónapot!");
   e = kerdez("Veszel egy vasrudat?");
     if (e == "igen"){
-    kiir ("Köszönöm!")
-    M = M - 5
-    kiir M("$-od maradt.")
-    v = v + 1
+    kiir ("Köszönöm!");
+    M = M - 5;
+    kiir M("$-od maradt.");
+    v = v + 1;
     }
     else (e == "nem") {
-        kiir ("Rendben, remélem máskor veszel!")
+        kiir ("Rendben, remélem máskor veszel!");
     }
- kiir v(" vasrudad van.");
+ kiir (v)("vasrudad van.");
 }
 })
