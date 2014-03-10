b0f0f91f84cb8a9e4065eb5bc27bbfe5da717f96
Szluka <szlukaandras@gmail.com>
2014-03-10 14:00:15 +0100

    Update and rename 11x4-kovacs.js to üdvözlő.js

---
+++ mezok/üdvözlő.js
@@ -0,0 +1,12 @@
+mezo({
+  vizszintes: 1,
+  fuggoleges: 1,
+  nev: 'andrask',
+  leiras: 'k',
+  kep: 'sporeicon.png',
+ralepeskor: function() {
+ kiir ("Idegen!-köszöntelek bolgyónkon!");
+ kiir ("Érezd jól magad! a fel,le,jobbra,balra nyilakkal udsz mozogni");
+  e = kerdez("");
+}
+})
