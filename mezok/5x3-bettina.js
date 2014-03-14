8beab726c78ccbb482dd0e25798a63d7255dc039
Bettina2003 <szbetti2003@gmail>
2014-03-14 15:37:49 +0100

    Update 5x3-bettina.js

--- mezok/5x3-bettina.js
+++ mezok/5x3-bettina.js
@@ -3,15 +3,34 @@ mezo({
   fuggoleges: 4,
   nev: ' Betti Házikója',
   leiras: 'Kicsi házikó.',
-  ralepeskor: function() {
-    kiir("Bettina házában vagy!");
+ // ralepeskor: function() {
+   // kiir("Bettina házában vagy!");
     
-    nev = kerdez ("Mi a keresztneved?");
-    udvozlet = "szia "+nev;
-    kiir (udvozlet);
-    kerdez1 = kerdez ("Hány éves vagy?");
-    kerdez2 = kerdez ("Hanyadik osztályos vagy?");
-    kerdez3 = kerdez ("Mi a kedvenc színed?");
-    kerdez4 = kerdez ("Szeretsz iskolába járni?");
+    //nev = kerdez ("Mi a keresztneved?");
+    //udvozlet = "szia "+nev;
+   // kiir (udvozlet);
+    //kerdez1 = kerdez ("Hány éves vagy?");
+   // kerdez2 = kerdez ("Hanyadik osztályos vagy?");
+    //kerdez3 = kerdez ("Mi a kedvenc színed?");
+    //kerdez4 = kerdez ("Szeretsz iskolába járni?");
+    
+     ralepeskor: function() {
+
+    var udvozlesUtan = function() {
+      kerdezuj("Hányszor írjam ki hogy neumann club ?", kezelAlmaValasz);
+    }
+
+    var kezelAlmaValasz = function(valasz) {
+      // Ciklus, haromszor hajtodik vegre
+      for (szamlalo = 0; szamlalo < valasz; szamlalo++) {
+        kiiruj("neumann club")
+      }
+    }
+
+    kiiruj("Szia latogato!", udvozlesUtan);
+    
+    
+    
+    
   }
 });
