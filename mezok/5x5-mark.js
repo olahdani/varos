317726a2828f4838d28481c374fbefb6da9a1372
markk55 <ATordaiMark@gmail.com>
2014-03-14 15:42:46 +0100

    Update 5x5-mark.js

--- mezok/5x5-mark.js
+++ mezok/5x5-mark.js
@@ -7,13 +7,13 @@ mezo({
   ralepeskor: function() {
     
    var udvozlesUtan = function() {
-  kerdezuj("Hany almát szeretnél", kezelAlmaValasz);
+  kerdezuj("Hany csokit szeretnél", kezelAlmaValasz);
    }
   
   var kezelAlmaValasz = function(valasz) {
     // Ciklus, haromszor hajtodik vegre
     for (szamlalo = 0; szamlalo < valasz; szamlalo++) {
-       kiiruj("Alma")
+       kiiruj("csoki")
     }
   }
 
