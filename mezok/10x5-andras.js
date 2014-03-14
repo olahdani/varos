cbd21c023210e3c84cded57c76ed3e3b0e64d435
Szluka <szlukaandras@gmail.com>
2014-03-14 17:09:22 +0100

    Update 10x5-andras.js

--- mezok/10x5-andras.js
+++ mezok/10x5-andras.js
@@ -16,12 +16,38 @@ ralepeskor: function() {
          sz = kerdez ("Kisebb mint 8?");
           if (sz = igen) {
             sz = kerdez ("Kisebb mint 7?");
-                      }
-          else (sz = ("nem"))("Te a 8-es számra gondoltál!")
+              if (sz = igen) {
+                sz = kerdez ("Kisebb mint 6?");
+                    if (sz = igen) {
+                      sz = kerdez ("Kisebb mint 5?");
+                          if (sz = igen) {
+                            sz = kerdez ("Kisebb mint 4?");
+                             if (sz = igen) {
+                               sz = kerdez ("Kisebb mint 3?");
+                                if (sz = igen) {
+                                  sz = kerdez ("Kisebb mint 2?");
+                                    if (sz = igen) {
+                                      sz = kerdez ("Kisebb mint 1?");
+                                      
+                                    else (sz = ("nem"))("Te az 1-es számra gondoltál!");
+                                                   }
+                                  else (sz = ("nem"))("Te a 2-es számra gondoltál!");
+                                              }
+                              else (sz = ("nem"))("Te a 3-es számra gondoltál!");
+                                            }          
+                            else (sz = ("nem"))("Te a 4-es számra gondoltál!");
+                                         }
+                        else (sz = ("nem"))("Te az 5-es számra gondoltál!");
+                                   }
+                else (sz = ("nem"))("Te a 6-es számra gondoltál!");
+                             }
+              else (sz = ("nem"))("Te a 7-es számra gondoltál!");
+                        }
+          else (sz = ("nem"))("Te a 8-es számra gondoltál!");
                       }          
-       else (sz = ("nem"))("Te a 9-es számra gondoltál!")
+       else (sz = ("nem"))("Te a 9-es számra gondoltál!");
                    }
-    else (sz = ("nem"))("Te a 10-es számra gondoltál!")
+    else (sz = ("nem"))("Te a 10-es számra gondoltál!");
 }
 })
 
