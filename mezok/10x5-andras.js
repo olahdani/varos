fd8e74937db7157766c2e2dbee79ae9cf93544a9
Szluka <szlukaandras@gmail.com>
2014-03-14 17:03:03 +0100

    Update 10x5-andras.js

--- mezok/10x5-andras.js
+++ mezok/10x5-andras.js
@@ -7,10 +7,21 @@ kep: 'figura.png',
 ralepeskor: function() {
  kiir ("Üdv idegen!");
  nev = kerdez("Mi a neved?");
-   sz = kerdez("Hányszor írjam ki a neved?");
-   for (szamlalo = 0; szamlalo < sz; szamlalo++) {
-      kiir (nev)
-    }
+   kiir("Szia, ") +nev ("!");
+   kiir ("Gondolj egy számra! (1-től-10-ig)");
+   sz = kerdez ("Kisebb mint 10? (Ezeket írhatod: igen ; nem ;)");
+    if (sz = igen) {
+      sz = kerdez ("Kisebb mint 9?");
+       if (sz = igen) {
+         sz = kerdez ("Kisebb mint 8?");
+          if (sz = igen) {
+            sz = kerdez ("Kisebb mint 7?");
+                      }
+          else (sz = ("nem"))("Te a 8-es számra gondoltál!")
+                      }          
+       else (sz = ("nem"))("Te a 9-es számra gondoltál!")
+                   }
+    else (sz = ("nem"))("Te a 10-es számra gondoltál!")
 }
 })
 
