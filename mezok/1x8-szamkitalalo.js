39a70fe690f4398a12307e3ee88c5cbabea2790f
Attila Maczak <attila@maczak.hu>
2014-03-28 09:48:46 -0400

    Update 1x8-szamkitalalo.js

--- mezok/1x8-szamkitalalo.js
+++ mezok/1x8-szamkitalalo.js
@@ -7,7 +7,7 @@ mezo({
   ralepeskor: function() {
     kiir ("Gondolj egy számra! (1-től-10-ig)");
     for (var i = 10; i > 0; i--) {
-      var valasz = kerdez ("Kisebb mint 10? (Ezeket írhatod: igen ; nem ;)");
+      var valasz = kerdez ("Kisebb mint "+ i + "? (Ezeket írhatod: igen ; nem ;)");
       if (valasz != 'igen') {
         kiir("Te a " + i + " számra gondoltál!");
         return;
